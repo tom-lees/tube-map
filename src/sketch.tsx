d902fe544a1bfea22379418ca69b5ebb1c34eb94
tom-lees <thomas.lees@outlook.com>
2025-07-27 14:18:32 +0100

    Remove P5.js; Add three.js; Setup basic three visualisation

--- src/sketch.tsx
+++ src/sketch.tsx
@@ -1,34 +1,35 @@
-import { useRef, useEffect } from "react";
-import p5 from "p5";
+// src/sketch.tsx
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
-const Sketch = () => {
-  const sketchRef = useRef<HTMLDivElement>(null);
+function SpinningBox(props: JSX.IntrinsicElements["mesh"]) {
+  const ref = useRef<THREE.Mesh>(null);
 
-  useEffect(() => {
-    const sketch = (p: p5) => {
-      p.setup = () => {
-        if (sketchRef.current) {
-          p.createCanvas(p.windowWidth, p.windowHeight).parent(
-            sketchRef.current
-          );
-        }
-      };
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.y += 0.01;
+      ref.current.rotation.x += 0.005;
+    }
+  });
 
-      p.draw = () => {
-        p.background(200);
-        p.fill(0);
-        p.ellipse(p.mouseX, p.mouseY, 50);
-      };
-    };
+  return (
+    <mesh ref={ref} {...props}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
+  );
+}
 
-    const p5Instance = new p5(sketch);
-
-    return () => {
-      p5Instance.remove(); // cleanup
-    };
-  }, []);
-
-  return <div ref={sketchRef} />;
-};
-
-export default Sketch;
+export default function Sketch() {
+  return (
+    <Canvas style={{ height: "100vh" }}>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[5, 5, 5]} />
+      <SpinningBox position={[0, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
+  );
+}
